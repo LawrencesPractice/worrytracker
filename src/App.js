ad3471c67abe03e886e0f6885dd387176b042a17
Lawrence Tsui <tbby28@gmail.com>
2023-08-14 20:28:16 +1000

    delete button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import WorryInput from './WorryInput';
 import WorryList from './WorryList';
 import Visualization from './Visualization';
-import { Container, Typography } from '@material-ui/core';
+import { Container, Typography, Button } from '@material-ui/core';
 
 function App() {
   const [worries, setWorries] = useState([]);
@@ -11,14 +11,27 @@ function App() {
     setWorries([...worries, worry]);
   };
 
+  const deleteWorry = (index) => {
+    setWorries((prevWorries) => {
+      const updatedWorries = [...prevWorries];
+      updatedWorries.splice(index, 1);
+      return updatedWorries;
+    });
+  };
+
+  const totalWorries = worries.length;
+
   return (
     <Container maxWidth="md">
       <Typography variant="h2" align="center" gutterBottom>
         Worry Tracker
       </Typography>
       <WorryInput addWorry={addWorry} />
-      <WorryList worries={worries} />
+      <WorryList worries={worries} deleteWorry={deleteWorry} />
       <Visualization worries={worries} />
+      <Typography variant="body1" align="center" gutterBottom>
+        Total Worries: {totalWorries}
+      </Typography>
     </Container>
   );
 }
--- src/WorryList.js
+++ src/WorryList.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import { List, ListItem, ListItemText, IconButton } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-function WorryList({ worries }) {
+function WorryList({ worries, deleteWorry }) {
   return (
     <List>
       {worries.map((worry, index) => (
         <ListItem key={index}>
           <ListItemText primary={worry.worry} secondary={`Rating: ${worry.rating}`} />
+          <IconButton edge="end" aria-label="delete" onClick={() => deleteWorry(index)}>
+            <DeleteIcon />
+          </IconButton>
         </ListItem>
       ))}
     </List>
